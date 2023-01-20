703d1d06a763b0c28481e82abc234945578e4f2c
Shreeram S <testinggrounds221.com>
2023-01-20 14:32:39 +0530

    first commit

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://user:pass@cluster0.ap8k7tq.mongodb.net/?retryWrites=true&w=majority', {
+	keepAlive: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+mongoose.set('debug', true);
+mongoose.Promise = Promise;
+
+module.exports.Todo = require("./todo");
+
+
+
